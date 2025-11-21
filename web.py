f10658c271613f4e311e69baf2ac5fd61c882891
grosbagel93-cell <grosbagel93@gmail.com>
2025-11-21 05:42:18 +0100

    Integrate Flask with Telebot for webhook handling

--- web.py
+++ web.py
@@ -1,4 +1,14 @@
-# ------- HANDLER START -------
+import telebot
+from flask import Flask, request
+
+# ------------ TOKEN ------------
+API_TOKEN = "8587383482:AAHnoeBYhluich_l00TCiIGfkn10MgSBYR8"
+bot = telebot.TeleBot(API_TOKEN)
+
+# ------------ FLASK APP ------------
+app = Flask(__name__)
+
+# ------------ HANDLER START ------------
 @bot.message_handler(commands=['start'])
 def start(message):
     keyboard = telebot.types.InlineKeyboardMarkup()
@@ -41,3 +51,15 @@ def start(message):
         ),
         reply_markup=keyboard
     )
+
+# ------------ WEBHOOK ------------
+@app.route('/', methods=['POST'])
+def webhook():
+    json_str = request.stream.read().decode("utf-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "OK", 200
+
+# ------------ RUN SERVER ------------
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
